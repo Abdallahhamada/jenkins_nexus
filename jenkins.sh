cbdcdcc77d1e7aa4018f65acce184e7639698117
Abdallah Hamada <73825634+Abdallahhamada@users.noreply.github.com>
2024-03-13 17:36:10 +0200

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "start docker build"
-docker build --tag "172.17.0.3:8084/jenkins:1.0" .
-echo $PASSWORD | docker login -u $USERNAME --password-stdin 172.17.0.3:8084
-docker push 172.17.0.3:8084/jenkins:1.0
+docker build --tag "http://localhost:8084/jenkins:1.0" .
+echo $PASSWORD | docker login -u $USERNAME --password-stdin http://localhost:8084
+docker push http://localhost:8084/jenkins:1.0
